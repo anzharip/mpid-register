bf2126238b6a86b2c6b93cd6e69e02def6f03b34
anzhari.p@gmail.com <anzhari.p@gmail.com>
2017-10-31 13:52:07 +0700

    Revise variable name

--- app.py
+++ app.py
@@ -12,39 +12,39 @@ driver.implicitly_wait(3)
 driver.get(url)
 
 # Find element based on name, then enter the keys
-elem = driver.find_element_by_name("nama")
-elem.clear()
-elem.send_keys(nama)
+el_nama = driver.find_element_by_name("nama")
+el_nama.clear()
+el_nama.send_keys(nama)
 
-elem = driver.find_element_by_name("username")
-elem.clear()
-elem.send_keys(username)
+el_username = driver.find_element_by_name("username")
+el_username.clear()
+el_username.send_keys(username)
 print(username)
 
-elem = driver.find_element_by_name("password")
-elem.clear()
-elem.send_keys(password)
+el_password = driver.find_element_by_name("password")
+el_password.clear()
+el_password.send_keys(password)
 
-elem = driver.find_element_by_name("cfmPassword")
-elem.clear()
-elem.send_keys(cfmPassword)
+el_cfmPassword = driver.find_element_by_name("cfmPassword")
+el_cfmPassword.clear()
+el_cfmPassword.send_keys(cfmPassword)
 
-elem = driver.find_element_by_name("dob")
-elem.clear()
-elem.send_keys(dob)
+el_dob = driver.find_element_by_name("dob")
+el_dob.clear()
+el_dob.send_keys(dob)
 
 # Find element based on name, instantiate Select class, then change the option value
-elem = driver.find_element_by_name("gender")
-select = Select(elem)
+el_gender = driver.find_element_by_name("gender")
+select = Select(el_gender)
 select.select_by_value(gender)
 
-elem = driver.find_element_by_name("handphone")
-elem.clear()
-elem.send_keys(handphone)
+el_handphone = driver.find_element_by_name("handphone")
+el_handphone.clear()
+el_handphone.send_keys(handphone)
 
 # Find element based on name, then click the element
-elem = driver.find_element_by_name("tou")
-elem.click()
-elem.send_keys(u'\ue007')
+el_tou = driver.find_element_by_name("tou")
+el_tou.click()
+el_tou.send_keys(u'\ue007')
 
 driver.close()
